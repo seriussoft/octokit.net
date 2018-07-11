52124748ed60f5e500b4088f796d834be9449b37
Ryan Gribble <ryan.gribble@gmail.com>
2018-07-11 23:52:15 +1000

    forgot integration tests for RepositoryId methods

--- Octokit.Tests.Integration/Reactive/ObservableCheckRunsClientTests.cs
+++ Octokit.Tests.Integration/Reactive/ObservableCheckRunsClientTests.cs
@@ -32,18 +32,33 @@ namespace Octokit.Tests.Integration.Reactive
                     var featureBranch = await Helper.CreateFeatureBranch(repoContext.RepositoryOwner, repoContext.RepositoryName, headCommit.Sha, "my-feature");
 
                     // Create a check run for the feature branch
-                    var newCheckRun = new NewCheckRun("status", featureBranch.Object.Sha)
+                    var newCheckRun = new NewCheckRun("name", featureBranch.Object.Sha)
                     {
-                        Status = CheckStatus.Queued,
-                        Output = new CheckRunOutput("My output", "The summary goes here")
-                        {
-                            Images = new List<CheckRunImage>()
-                            {
-                                new CheckRunImage("banana", "https://imgur.com/banana")
-                            }
-                        }
+                        Status = CheckStatus.Queued
                     };
                     var result = await _githubAppInstallation.Check.Run.Create(repoContext.RepositoryOwner, repoContext.RepositoryName, newCheckRun);
+
+                    // Check result
+                    Assert.NotNull(result);
+                    Assert.Equal(featureBranch.Object.Sha, result.HeadSha);
+                }
+            }
+
+            [GitHubAppsTest]
+            public async Task CreatesCheckRunWithRepositoryId()
+            {
+                using (var repoContext = await _github.CreateRepositoryContext(new NewRepository(Helper.MakeNameWithTimestamp("public-repo")) { AutoInit = true }))
+                {
+                    // Create a new feature branch
+                    var headCommit = await _github.Repository.Commit.Get(repoContext.RepositoryId, "master");
+                    var featureBranch = await Helper.CreateFeatureBranch(repoContext.RepositoryOwner, repoContext.RepositoryName, headCommit.Sha, "my-feature");
+
+                    // Create a check run for the feature branch
+                    var newCheckRun = new NewCheckRun("name", featureBranch.Object.Sha)
+                    {
+                        Status = CheckStatus.Queued
+                    };
+                    var result = await _githubAppInstallation.Check.Run.Create(repoContext.RepositoryId, newCheckRun);
 
                     // Check result
                     Assert.NotNull(result);
